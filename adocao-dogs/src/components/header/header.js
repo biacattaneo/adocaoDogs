4f2450ac565ff912714a4f85c9dd4a666e9a0ae2
megazortti <39544599+megazortti@users.noreply.github.com>
2021-11-29 21:39:51 -0300

    Another update

--- adocao-dogs/src/components/header/header.js
+++ adocao-dogs/src/components/header/header.js
@@ -4,16 +4,18 @@ import { NavLink } from "react-router-dom";
 
 const Header = () => {
     return (
-        <header className="Header">
-            <nav>
-                <ul>
-                    <li><NavLink to="/home" exact>Home</NavLink></li>
-                    <li><NavLink to="/cadastro" exact>Cadastro</NavLink></li>
-                    <li><NavLink to="/login" exact>Login</NavLink></li>
-                    <li><NavLink to="/cadastrodog" exact>Cadastrar um dog</NavLink></li>
-                </ul>
-            </nav>
-        </header>
+        <view>
+            <header className="Header">
+                <nav>
+                    <ul>
+                        <li><NavLink to="/home" exact>Home</NavLink></li>
+                        <li><NavLink to="/cadastro" exact>Cadastro</NavLink></li>
+                        <li><NavLink to="/login" exact>Login</NavLink></li>
+                        <li><NavLink to="/cadastrodog" exact>Cadastrar um dog</NavLink></li>
+                    </ul>
+                </nav>
+            </header>
+        </view>
     );
 };
 
