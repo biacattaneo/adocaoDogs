101313645dc8d7c75411165bb9d32ee2edec3356
megazortti <39544599+megazortti@users.noreply.github.com>
2021-11-30 22:24:35 -0300

    Another Commit

--- adocao-dogs/src/components/cadastroDog/cadastro.js
+++ adocao-dogs/src/components/cadastroDog/cadastro.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import './cadastro.css';
 import Home, {adicionarCachorro} from '../home/home';
+import axios from 'axios';
+var querystring = require('querystring');
+
+const api = axios.create({
+    baseURL: "http://localhost:8888"
+})
 
 const Cadastro = (props) => {
+    function handleSubmit(e){
+        e.preventDefault();
+        api({
+            method: 'post',
+            url: '/set_cachorro',
+            data: {
+              dogName: e.target.elements.username.value,
+              dogRaca: e.target.elements.raca.value,
+              dogImagem: e.target.elements.imagem.value,
+              dogPorte: e.target.elements.porte.value,
+              dogIdade: e.target.elements.idade.value
+            }
+          });
+
+    }
     return (
         <div className="cadastro">
             <h3>Cadastro</h3>
-            <form>
+            <form onSubmit={handleSubmit} >
                 <div>
                     <div>
                         <label>Nome</label>
@@ -14,7 +35,7 @@ const Cadastro = (props) => {
                 </div>
                 <div>
                     <div>
-                        <input type="text" required className="campo" id="nameAdotante" placeholder="Bolinha"></input>
+                        <input type="text" name="username" className="campo"  placeholder="Bolinha"></input>
                     </div>
                 </div>
                 <div>
@@ -24,7 +45,7 @@ const Cadastro = (props) => {
                 </div>
                 <div>
                     <div>
-                        <input required type="text" className="campo" id="contatoAdotante" placeholder="Vira-lata"></input>
+                        <input name="raca" type="text" className="campo"  placeholder="Vira-lata"></input>
                     </div>
                 </div>
                 <div>
@@ -34,7 +55,7 @@ const Cadastro = (props) => {
                 </div>
                 <div>
                     <div>
-                        <input required type="file" accept=".jpg,.jpeg,.png"className="campo" id="emailAdotante" placeholder="Digite o URL da imagem"></input>
+                        <input type="file" name="imagem" accept=".jpg,.jpeg,.png" className="campo"  placeholder="Digite o URL da imagem"></input>
                     </div>
                 </div>
                 <div>
@@ -44,7 +65,7 @@ const Cadastro = (props) => {
                 </div>
                 <div>
                     <div>
-                        <input required type="text" className="campo" placeholder="Grande" id="passwordAdotante" ></input>
+                        <input name="porte" type="text" className="campo" placeholder="Grande"  ></input>
                     </div>
                 </div>
                 <div>
@@ -54,13 +75,13 @@ const Cadastro = (props) => {
                 </div>
                 <div>
                     <div>
-                        <input required type="number" placeholder="3" className="campo" id="confirmPasswordAdotante" ></input>
+                        <input  name="idade" type="number" placeholder="3" className="campo"  ></input>
                     </div>
                 </div>
                 <div>
                     <div>
                         {/* <input required type="submit" value="Cadastrar" ></input> */}
-                        <button type="submit" >Enviar</button>
+                        <button type="submit">Enviar</button>
                     </div>
                 </div>
             </form>
--- back-end/app.js
+++ back-end/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
-const app = express();
+const db = require('./config/dbconnection')
 var cors = require('cors');
 
+const app = express();
+
 app.use(cors({origin: true, credentials: true}));
+app.use(express.urlencoded({ extended: true })); 
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send(JSON.stringify({
@@ -10,5 +14,18 @@ app.get('/', (req, res) => {
         nome: 'Victor Mazzotti'
     }));
 })
+app.get('/get_cachorros', (req, res) => {
+    db.query('SELECT * FROM cachorro').then((i)=>{
+        res.send(JSON.stringify(i.rows));
+    })
+
+})
+app.post('/set_cachorro', (req, res) => {
+    console.log(req.body);
+    db.query(`INSERT INTO cachorro (nome_cachorro,raça,foto,porte,idade) VALUES ('${req.body.dogName}','${req.body.dogRaca}','${req.body.dogImagem}','${req.body.dogPorte}','${req.body.dogIdade}')`).then(()=>{
+        res.sendStatus(200);
+    });
+
+});
 
 app.listen(8888);
